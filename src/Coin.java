864ef62452c52dc880cbbaaf7cca17f66b8a25ba
Hannah Zabarsky <hello@hannahzabarsky.com>
2019-09-30 21:12:15 +0000

    WIP - Broken class, working on default no args constructor

--- src/Coin.java
+++ src/Coin.java
@@ -1,33 +1,49 @@
+import java.util.*;
+
 public class Coin {
-    //should not store past state, only current state
-    //double check conventions - private variables?
-    String sideUp;
+        public Coin(String sideUp){
+        sideUp = "heads";
+        boolean headsOrTailsBoolean = randomValue.nextBoolean();
+
+        if (headsOrTailsBoolean) {
+            sideup = "heads";
+        }
+        else {
+            sideup = "tails";
+
+        }
+
+        public static void main(String args[]) {
+        //should not store past state, only current state
+        //double check conventions - private variables?
+        Random randomValue = new Random();
 
 
-    //should have following methods
+        System.out.println(sideUp);
 
-    //A no-args constructor that randomly determines the side of the coin that is facing up
-    //("heads" || "tails") and initializes sideUp accordingly
-    Coin() {
-        System.out.println("Is this working?");
-    }
 
-    //A void method named toss that simulates the tossing of the coin. When the toss
-    //method is called, it randomly determines the side of the coin that is facing up ("heads || "tails)
-    //and sets the sideUp field accordingly
-    public void toss() {
-         // boolean tossResult = boolean.rand - this isn't the right syntax
+        //should have following methods
 
-    }
+        //MyClass myClassObj = new MyClass(); <- how to instantiate new object from class
 
-    //A method named getSideUp that returns the value of the sideUp field
-    private static double getSideUp() {
+
+        //A no-args constructor that randomly determines the side of the coin that is facing up
+        //("heads" || "tails") and initializes sideUp accordingly
+
+        }
+
+        //A void method named toss that simulates the tossing of the coin. When the toss
+        //method is called, it randomly determines the side of the coin that is facing up ("heads || "tails)
+        //and sets the sideUp field accordingly
+
+        //A method named getSideUp that returns the value of the sideUp field
+
         //this doesn't do anything yet either
-        return 0;
-    }
 
-    public static void main( String args) {
-        new Coin();
-        //this doesn't do anything but it's the main output from the class
+
+            //this doesn't do anything but it's the main output from the class
+
+            // boolean tossResult = boolean.rand - this isn't the right syntax
+
     }
 }
--- src/Player.java
+++ src/Player.java
@@ -1,7 +1,25 @@
 public class Player {
+    public static void main(String args[]){
+    String guess = "heads";
+    int score = 0;
+    String sideUp = "heads";
+        System.out.println("Testing");
+    //if guess.equals(coin.sideup)
+        if (guess.equals(sideUp)) {
+                score++;
+                System.out.println("Your score is " + score);
+        }
+        else {
+            score = score--;
+            System.out.println(score);
+        }
+    }
+
+
     //has a current guess
     //has a current score - needs to be cumulative
     //if current guess is correct - add 1 to score
     //if current guess is incorrect - subtract 1 from score.
     //does a player go to negative for guesses? or does it stay at zero like finite amount of money
+
 }
